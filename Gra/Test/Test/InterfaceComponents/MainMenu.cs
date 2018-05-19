35a839e96a8121a98d2b88b9674db52419ca11dc
SeNaXpl <kubanycz.gsm@gmail.com>
2018-05-19 15:23:38 +0200

    Początkowy commit

---
+++ Gra/Test/Test/InterfaceComponents/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.InterfaceComponents
+{
+    public static class MainMenu
+    {
+        public static void DisplayMenu()
+        {
+            Console.WriteLine("1. Nowa gra");
+            Console.WriteLine("2. Wczytaj gre");
+            Console.WriteLine("3. Opcje");
+            Console.WriteLine("4. Wyjdz");
+        }
+
+        public static void NewGame()
+        {
+            Console.WriteLine("\nWybrano nowa gra");
+            //Tworzenie postaci
+        }
+
+        public static void LoadGame()
+        {
+            Console.WriteLine("\nWybrano wczytaj gre");
+            //Wybór save
+        }
+
+        public static void Settings()
+        {
+            Console.WriteLine("\nWybrano opcje");
+            //Wyświetl opcje
+        }
+    }
+}
---
+++ Gra/Test/Test/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.InterfaceComponents;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char selection = '0';
+            int selectionConverted = 0;
+
+            while (selectionConverted!= 4)
+            {
+                MainMenu.DisplayMenu();
+                selection = Console.ReadKey().KeyChar;
+
+                if (int.TryParse(selection.ToString(), out selectionConverted))
+                {
+                    switch (selectionConverted)
+                    {
+                        case 1:
+                            MainMenu.NewGame();
+                            break;
+                        case 2:
+                            MainMenu.LoadGame();
+                            break;
+                        case 3:
+                            MainMenu.Settings();
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nWybrano zla wartosc!");
+                    
+                }
+
+            }
+        }
+    }
+}
